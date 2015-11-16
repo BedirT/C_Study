43ba333758756c3f2ccb706da6eafa0af21607da
BedirT <bedir.tapkan.53@gmail.com>
2015-11-16 16:19:51 -0600

    added assignment 4

--- study/sorting2.c
+++ study/sorting2.c
@@ -2,50 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
+void initializeArray(int numbers[]);
+void indexSort (int numbers[], int indexes[]);
 
 int main() {
 	
-	int i, temp, sort;
-	int number;
-
-	printf("Please enter how many digit you will enter?\n");
-	scanf(" %d", &number);
-
-	int array[number];
-
-	for( i = 0 ; i<number  ; i++){
-
-		printf("Please enter %d. number\n", i+1);
-		scanf(" %d", &array[i]);
-	}
-	
-	while(1){
-		
-		sort = 0;
-
-		for ( i = 0 ; i < number-1  ; i++ ){
-
-			if( array[i] < array[i+1] ){
-				
-				temp = array[i];
-				array[i] = array [i+1];
-				array[i+1] = temp;
-				sort = 1;
-
-			}
-		}
-			
-		if(sort == 0){
-			break;
-		}
-	}
+    int i;
+    
+    srand(time(NULL));
+    
+    int myArray[9];
+    int sortedArray[9];
+    
+    initializeArray(myArray);
+    
+    printf("\nNORMAL LIST\n");
+    printf("-----------\n");
+    
+    for (i=0 ; 10>i ; i++){
+        printf("%d\n", myArray[i]);
+    }printf("\n");
+    
+	indexSort (myArray, sortedArray);
 
 	printf("\nSORTED LIST\n");
 	printf("-----------\n");
 
-	for (i=0 ; number>i ; i++){
-		printf("%d\n", array[i]);
+	for (i=0 ; 10>i ; i++){
+		printf("%d\n", sortedArray[i]);
 	}printf("\n");
 
 	return 0;
 }
+
+void initializeArray(int numbers[]){
+    
+    for( int i = 0 ; i<10  ; i++){
+        numbers[i] = rand()%100;
+    }
+    
+}
+
+void indexSort (int numbers[], int indexes[]){
+    int temp,sort;
+    
+    for( int i = 0 ; i<10  ; i++){
+        indexes[i] = numbers[i];
+    }
+    
+    while(1){
+        
+        sort = 0;
+        
+        for ( int i = 0 ; i < 9  ; i++ ){
+            
+            if( indexes[i] < indexes[i+1] ){
+                
+                temp = indexes[i];
+                indexes[i] = indexes [i+1];
+                indexes[i+1] = temp;
+                sort = 1;
+                
+            }
+        }
+        
+        if(sort == 0){
+            break;
+        }
+    }
+    
+    
+    
+}
+
+
