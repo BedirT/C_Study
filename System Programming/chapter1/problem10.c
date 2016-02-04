6c25e744ac89445229c94f65a7b9b5622357eeb7
BedirT <bedir.tapkan.53@gmail.com>
2016-02-04 12:13:45 -0600

    SP/

--- System Programming/chapter1/problem10.c
+++ System Programming/chapter1/problem10.c
@@ -5,33 +5,22 @@
 
 int main()
 {
-    int n[5],s,i,j,w;
-
+    int n[5]=0,s,i,j,w;
     for (i=0; i<5; i++){
-        
         printf("Enter any integer: ");
-        
         scanf("%d",&(n[i]));
-        
-        
-    
     }
     
     for (i=0; i<5; i++) {
-        
         s=0; /* find index of smallest */
-        
         for (j=1; j<=i; j++){
             if (n[j] < n[s])
                 s=j;
-            
         }
         w=n[i]; /* swap smallest with current */
         n[i]=n[s];
         n[s]=w;
     }
-    
     for (i=0; i<5; i++)
         printf("%d\n",n[i]);
 }
-
