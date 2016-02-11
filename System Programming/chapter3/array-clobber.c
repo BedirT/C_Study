7e5de6813a2b7c69b4dd0ed1aad9d09c879dad2f
BedirT <bedir.tapkan.53@gmail.com>
2016-02-11 15:16:26 -0600

    cobbler

--- System Programming/chapter3/array-clobber.c
+++ System Programming/chapter3/array-clobber.c
@@ -4,10 +4,10 @@
 
 #include <stdio.h>
 
-main()
+int main()
 {
 int a[2];	/* 2 cells, each cell 4 bytes (32 bits) */
-float b[3];	/* 3 cells, each cell 4 bytes (32 bits) */
+float b[4];	/* 3 cells, each cell 4 bytes (32 bits) */
 double c[4];	/* 4 cells, each cell 8 bytes (64 bits) */
 char d[5];	/* 5 cells, each cell 1 bytes (8 bits) */
 
@@ -16,7 +16,9 @@ b[1]=4.0;
 c[2]=14.7;
 d[4]='a';
 
-b[4]=15.9;
-printf("%lf\n",b[4]);
+printf("%p\n",&b[3]);
+
+return 0;
+
 }
 
